614eb9ccc92d389856837a32f27b9f777570d50b
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-24 03:46:36 +0100

    More updates

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -34,14 +34,14 @@ $(() => {
   function ud(val) {
     if (val == undefined) {
       return ""
-    }else {
+    } else {
       return val
     }
   }
   const arr = {};
   $.ajax({
     type: "POST",
-    url: "http://0.0.0.0:5001/api/v1/places_search",
+    url: "http://127.0.0.1:5001/api/v1/places_search",
     data: "{}",
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
@@ -60,14 +60,49 @@ $(() => {
       <div class="number_bathrooms">${ud(place.number_bathrooms)} Bathroom</div>
 	  </div>
 	  <div class="user">
-            <b>Owner:</b> ${ ud(place.user.first_name) } ${ ud(place.user.last_name) }
+            <b>Owner:</b> ${ud(place.user.first_name)} ${ud(place.user.last_name)}
           </div>
           <div class="description">
-	    ${ ud(place.description) }
+	    ${ud(place.description)}
           </div>
 	</article>
       `;
       $('section.places').append(template);
     }
   });
+
+  $(".filters button").click(() => {
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:5001/api/v1/places_search",
+      data: JSON.stringify({ amenities: variable }),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+    }).done((data) => {
+      $('section.places').empty();
+      for (const place of data) {
+        const template =
+          `
+      <article>
+	  <div class="title_box">
+	    <h2>${ud(place.name)}</h2>
+	    <div class="price_by_night">$${ud(place.price_by_night)}</div>
+	  </div>
+	  <div class="information">
+	    <div class="max_guest">${ud(place.max_guest)} Guest</div>
+      <div class="number_rooms">${ud(place.number_rooms)} Bedroom</div>
+      <div class="number_bathrooms">${ud(place.number_bathrooms)} Bathroom</div>
+	  </div>
+	  <div class="user">
+            <b>Owner:</b> ${ud(place.user.first_name)} ${ud(place.user.last_name)}
+          </div>
+          <div class="description">
+	    ${ud(place.description)}
+          </div>
+	</article>
+      `;
+        $('section.places').append(template);
+      }
+    })
+  })
 })
